de83b3f0c15fe53dc1937eb528ecd49eac3b7a91
Raymond DeVries <raymond.l.devries@gmail.com>
2023-10-29 16:18:22 -0700

    Refactor article linkes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,6 @@
 	import aboutData from './about.json';
 	import MarkDown from '$lib/MarkDown.svelte';
 	import Photo from '$lib/Photo.svelte';
-	import { goto } from '$app/navigation';
 </script>
 
 <svelte:head>
@@ -13,15 +12,11 @@
 <hr class="my-10 h-0.5 rounded border-0 bg-zinc-600" />
 <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
 	{#each aboutData.articles as { link, title, photo }}
-		<div
-			class="h-96 overflow-hidden rounded-xl bg-zinc-800"
-			role="button"
-			on:click={() => goto(link)}
-			on:keypress={() => goto(link)}
-			tabindex={0}
-		>
-			<header class="my-7 text-center text-lg"><b>{title}</b></header>
-			<Photo imgSrc={photo} altText="Stuff" class="h-full w-full object-cover" />
-		</div>
+		<a href={link}>
+			<div class="h-96 overflow-hidden rounded-xl bg-zinc-800">
+				<header class="my-7 text-center text-lg"><b>{title}</b></header>
+				<Photo imgSrc={photo} altText={`${title} article cover photo`} class="h-full w-full object-cover" />
+			</div>
+		</a>
 	{/each}
 </div>
