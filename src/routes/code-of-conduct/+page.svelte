1af38c371e545956ff2bca54ecc68639cfb485b5
Raymond DeVries <raymond.l.devries@gmail.com>
2024-10-15 21:12:18 -0700

    Add code of conduct

---
+++ src/routes/code-of-conduct/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import codeOfConduct from './code-of-conduct.json';
+	import MarkDown from '$lib/MarkDown.svelte';
+</script>
+
+<svelte:head>
+	<meta name="description" content={codeOfConduct.metaDescription} />
+	<title>Code of Conduct</title>
+</svelte:head>
+
+<MarkDown content={codeOfConduct.body} />
