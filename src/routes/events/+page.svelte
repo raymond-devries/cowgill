5e5fac9f0944b37320f599d6f373c654954e2244
Raymond DeVries <raymond.l.devries@gmail.com>
2024-11-16 15:51:15 -0800

    Add upcoming event tiles to the events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,7 +1,58 @@
 <script>
 	import eventsData from './events.json';
 	import MarkDown from '$lib/MarkDown.svelte';
+	import upcomingEvents from './upcoming_events.json';
+	import IconStrava from '~icons/bi/strava';
+
+	const formatDate = (dateString, timeZone = 'America/Los_Angeles') => {
+		const date = new Date(dateString);
+		const options = {
+			weekday: 'short',
+			hour: 'numeric',
+			minute: 'numeric',
+			hour12: true,
+			month: 'short',
+			day: 'numeric',
+			timeZone
+		};
+
+		// Format the date string and remove commas
+		const formattedDate = date.toLocaleString('en-US', options).replace(/,/g, '');
+		const [weekday, time, period, month, day] = formattedDate.split(' ');
+
+		// Construct the final formatted string with AM/PM included
+		return `${weekday} ${time} ${period.toUpperCase()} ${month} ${day}`;
+	};
+	console.log(upcomingEvents);
 </script>
+
+<h1 class="text-6xl">{eventsData.title}</h1>
+<hr class="my-10 h-0.5 rounded border-0 bg-zinc-600" />
+
+{#if upcomingEvents.length > 0}
+	<h3 class="mb-5 text-4xl">Upcoming Events</h3>
+	<div class="grid grid-cols-1 gap-6 md:grid-cols-2">
+		{#each upcomingEvents as { id, title, upcoming_occurrences, route: { map_urls: { dark_url } } }}
+			<a href={`https://www.strava.com/clubs/470714/group_events/${id}`}>
+				<div class="flex h-full flex-col overflow-hidden rounded-xl bg-zinc-800">
+					<header class="my-5 px-8 text-lg lg:px-10">
+						<div class="mb-2 flex items-center gap-x-2 sm:gap-x-4">
+							<IconStrava class="flex-none sm:text-2xl"></IconStrava>
+							<b class="md:text-xl">{title}</b>
+						</div>
+						<p class="text-sm sm:text-lg">{formatDate(upcoming_occurrences[0])}</p>
+					</header>
+					{#if dark_url}
+						<div class="mt-auto w-full">
+							<img src={dark_url} alt={`A map of ${title}`} class="w-full object-cover" />
+						</div>
+					{/if}
+				</div>
+			</a>
+		{/each}
+	</div>
+	<hr class="my-10 h-0.5 rounded border-0 bg-zinc-600" />
+{/if}
 
 <MarkDown content={eventsData.body} />
 
