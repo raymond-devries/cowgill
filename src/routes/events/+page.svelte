6d5b736ca8c09c40d24a9d3cfccfc8e763e68068
Raymond DeVries <raymond.l.devries@gmail.com>
2024-11-18 10:13:02 -0800

    Provide fill image for upcoming events

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -3,6 +3,7 @@
 	import MarkDown from '$lib/MarkDown.svelte';
 	import upcomingEvents from './upcoming_events.json';
 	import IconStrava from '~icons/bi/strava';
+	import CowgillLogo from '$lib/CowgillLogo.svelte';
 
 	const formatDate = (dateString, timeZone = 'America/Los_Angeles') => {
 		const date = new Date(dateString);
@@ -46,6 +47,10 @@
 						<div class="mt-auto w-full">
 							<img src={dark_url} alt={`A map of ${title}`} class="w-full object-cover" />
 						</div>
+					{:else}
+						<div class="mt-auto w-full h-[195.217px] bg-zinc-700">
+							<CowgillLogo class="mx-auto px-5 my-5 max-h-[140px]"></CowgillLogo>
+						</div>
 					{/if}
 				</div>
 			</a>
@@ -66,7 +71,7 @@
 	<a
 		class="btn-pink"
 		href="https://calendar.google.com/calendar/u/0/embed?src=cowgill.trail@gmail.com&ctz=America/Los_Angeles"
-		>View Calendar️</a
+	>View Calendar️</a
 	>
 	<a class="btn-pink" href="https://calendar.google.com/calendar/u/0?cid=Y293Z2lsbC50cmFpbEBnbWFpbC5jb20">Subscribe</a>
 </div>
