32db5c0bea357bed3b8700e562948ed3b3ff4c1d
Raymond DeVries <raymond.l.devries@gmail.com>
2025-01-22 21:09:55 -0800

    Instantiate boise content

--- src/lib/pageContent.js
+++ src/lib/pageContent.js
@@ -4,6 +4,12 @@ import SeattleEventsData from '$lib/content/seattle/events.json';
 import SeattleHomeData from '$lib/content/seattle/home.json';
 import SeattleMetaData from '$lib/content/seattle/meta.json';
 
+import BoiseAboutData from '$lib/content/Boise/about.json';
+import BoiseContactData from '$lib/content/Boise/contact.json';
+import BoiseEventsData from '$lib/content/Boise/events.json';
+import BoiseHomeData from '$lib/content/Boise/home.json';
+import BoiseMetaData from '$lib/content/Boise/meta.json';
+
 const seattlePageData = {
 	ABOUT: SeattleAboutData,
 	CONTACT: SeattleContactData,
@@ -12,8 +18,16 @@ const seattlePageData = {
 	META: SeattleMetaData
 };
 
+const boisePageData = {
+	ABOUT: BoiseAboutData,
+	CONTACT: BoiseContactData,
+	EVENTS: BoiseEventsData,
+	HOME: BoiseHomeData,
+	META: BoiseMetaData
+};
+
 export const pageData = {
 	SEATTLE: seattlePageData,
 	PORTLAND: seattlePageData,
-	BOISE: seattlePageData
+	BOISE: boisePageData
 };
