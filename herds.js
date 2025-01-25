e39747b4454f70d0c3bf9c87f00f9a4041748a88
Raymond DeVries <raymond.l.devries@gmail.com>
2025-01-24 18:40:34 -0800

    Generate config dynamically

---
+++ herds.js
@@ -0,0 +1,5 @@
+export const HERDS = [
+	'seattle',
+	'boise'
+	// "portland"
+];
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import Icons from 'unplugin-icons/vite';
 import { imagetools } from 'vite-imagetools';
 import { createFilter, dataToEsm } from '@rollup/pluginutils';
 import { getAverageColor } from 'fast-average-color-node';
+import generateFile from 'vite-plugin-generate-file';
+import { CMSconfig } from './tools/generate_admin.js';
+import { HERDS } from './herds.js';
 
 export function primarycolor() {
 	const filter = createFilter(/[^?]+\.(heic|heif|avif|jpeg|jpg|png|tiff|webp|gif)(\?(?=primarycolor=true).*)?$/);
@@ -23,6 +26,18 @@ export default ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 	const picRegex = new RegExp(`src\\/lib\\/content\\/${process.env.VITE_CONTENT_KEY.toLowerCase()}\\/*`);
 	return defineConfig({
-		plugins: [sveltekit(), Icons({ compiler: 'svelte' }), primarycolor(), imagetools({ include: picRegex })]
+		plugins: [
+			sveltekit(),
+			Icons({ compiler: 'svelte' }),
+			primarycolor(),
+			imagetools({ include: picRegex }),
+			generateFile([
+				{
+					type: 'yaml',
+					output: './admin/config.yml',
+					data: CMSconfig(HERDS)
+				}
+			])
+		]
 	});
 };
